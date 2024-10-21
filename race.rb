0e0ea0ddd3d4758c9e2b8352029447a24461f8a7
Roberto Barros <roberto.f.barros@gmail.com>
2024-10-21 13:41:15 -0300

    pseudocode

---
+++ race.rb
@@ -0,0 +1,13 @@
+# Exibe uma mensagem de boas vindas
+
+# Mostra os cavalos da corrida
+
+# Perguntar qual cavalo ela acha que vai ganhar
+
+# Simular a corrida com os cavalos e determinar a ordem de chegada
+
+# Mostrar o resultado da corrida
+
+# Verificar se o cavalo escolhido é o vencedor da corrida
+
+# Mostrar se ganhou ou perdeu
