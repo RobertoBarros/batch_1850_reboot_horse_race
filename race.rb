55f9f86d197b58e71f5790613c9bccf89ad2a08e
Caio Castelhano <castelhanoc@gmail.com>
2024-10-21 14:30:59 -0300

    horse race

--- race.rb
+++ race.rb
@@ -1,13 +1,50 @@
-# Exibe uma mensagem de boas vindas
+wallet = 100
 
-# Mostra os cavalos da corrida
+loop do 
 
-# Perguntar qual cavalo ela acha que vai ganhar
+  # Exibe uma mensagem de boas vindas
+  puts "Bem-vindos à corrida de cavalos!" 
 
-# Simular a corrida com os cavalos e determinar a ordem de chegada
+  # Mostra os cavalos da corrida
+  horses = ["preto", "branco", "malhado"]
+  horses.each_with_index do |horse, index|
+    puts "#{index + 1} - #{horse}"
+  end
 
-# Mostrar o resultado da corrida
+  # Perguntar qual cavalo ela acha que vai ganhar
+  puts "Escolha o número do cavalo:"
+  number = gets.chomp.to_i
 
-# Verificar se o cavalo escolhido é o vencedor da corrida
+  # Simular a corrida com os cavalos e determinar a ordem de chegada
+  podium = horses.shuffle
 
-# Mostrar se ganhou ou perdeu
+  # Mostrar o resultado da corrida
+  puts "O resultado da corrida foi:"
+  podium.each_with_index do |horse, index|
+    puts "#{index + 1} - #{horse}"
+  end
+
+  # Verificar se o cavalo escolhido é o vencedor da corrida
+  puts "O cavalo vencedor é o #{podium[0]}."
+  puts "Você apostou no cavalo #{horses[number - 1]}."
+
+  # Mostrar se ganhou ou perdeu
+  # puts podium[0] == horses[number - 1] ? "Você venceu!" : "Você perdeu!"
+
+  if podium[0] == horses[number - 1] 
+    puts "Você venceu!" 
+    wallet +=10
+  else
+    puts "Você perdeu!"
+    wallet -=30
+  end
+
+  puts "O seu saldo é de #{wallet} reais."
+
+  if wallet <= 0 
+    break
+  end
+
+end
+
+puts "Você está falido."
